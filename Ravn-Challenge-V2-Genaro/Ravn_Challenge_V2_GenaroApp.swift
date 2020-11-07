6e0706c13a1494fc70bc1172aeef0b8f5ccce0be
Genaro <gena_albert@hotmail.com>
2020-11-06 19:06:44 -0600

    Initial Commit

---
+++ Ravn-Challenge-V2-Genaro/Ravn_Challenge_V2_GenaroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ravn_Challenge_V2_GenaroApp.swift
+//  Ravn-Challenge-V2-Genaro
+//
+//  Created by Genaro Alvarenga on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Ravn_Challenge_V2_GenaroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
