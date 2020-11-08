be9a6b1dba90badcb06f01748d3ce088c58a4a22
Genaro <galvarenga.developer@gmail.com>
2020-11-08 15:09:12 -0600

    :globe_with_meridians: Localize strings

---
+++ Ravn-Challenge-V2-Genaro/SupportFiles/Localize/Localizable.swift
@@ -0,0 +1,19 @@
+//
+//  Localizable.swift
+//  Ravn-Challenge-V2-Genaro
+//
+//  Created by Genaro Alvarenga on 11/8/20.
+//
+
+import Foundation
+
+enum Localizable {
+    static let peopleHeader = NSLocalizedString("People.Header", comment: "The navbar header of the People view")
+    static let loading = NSLocalizedString("Loading", comment: "The Loading word")
+    static let error = NSLocalizedString("Error", comment: "The Error word")
+    static let close = NSLocalizedString("Close", comment: "The Close word")
+    static let detailGeneralTitle = NSLocalizedString("Detail.General.Title",
+                                                      comment: "The title of the general section inside the detail view")
+    static let detailVehiclesTitle = NSLocalizedString("Detail.Vehicles.Title",
+                                                      comment: "The title of the vehicles section inside the detail view")
+}
